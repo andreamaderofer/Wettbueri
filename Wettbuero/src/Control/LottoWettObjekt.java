1f63e6977ad8418d86fc71b5da671fc7597b08f3
Andrea <Andrea@Andrea-Laptop.mshome.net>
2016-05-28 13:53:43 +0200

    Andrea

--- Wettbuero/src/Control/LottoWettObjekt.java
+++ Wettbuero/src/Control/LottoWettObjekt.java
@@ -1,31 +1,24 @@
 package Control;
 
-public class LottoWettObjekt 
-{
-	protected int[] zahl;
+public class LottoWettObjekt {
+	protected int[] zahl=new int[500];
 
-	public LottoWettObjekt()
-	{
-		
+	public LottoWettObjekt() {
+
 	}
 
-	public LottoWettObjekt (String pars) 
-	{
-		
+	public LottoWettObjekt(String pars) {
 	}
 
-	public LottoWettObjekt (int[] zahlen) 
-	{
-		
+	public LottoWettObjekt(int[] zahlen) {
+		this.zahl = zahlen;
 	}
 
-	protected void start ()
-	{
-		
+	protected void start() {
+
 	}
 
-	protected void end () 
-	{
-		
+	protected void end() {
+
 	}
 }
