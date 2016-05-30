92ba02b6385a5ac0aef1bf08d2fdf8c1e8980b5e
Tobias <Tobias@192.168.0.102>
2016-05-30 08:52:46 +0200

    Lottowettobjekt angefangen und Überlegungen angestellt

--- Wettbuero/src/Control/LottoWettObjekt.java
+++ Wettbuero/src/Control/LottoWettObjekt.java
@@ -1,24 +1,33 @@
 package Control;
 
 public class LottoWettObjekt {
-	protected int[] zahl=new int[500];
-
-	public LottoWettObjekt() {
-
+	protected int[] zahlen = new int[500];
+	
+	public LottoWettObjekt() 
+	{
 	}
 
-	public LottoWettObjekt(String pars) {
+	public LottoWettObjekt(String pars) 
+	{
+		for(int i=0; i<=zahlen.length;i++)
+		{
+			zahlen[i] = Integer.parseInt(pars.split(" ").toString());
+		}
 	}
 
-	public LottoWettObjekt(int[] zahlen) {
-		this.zahl = zahlen;
+	public LottoWettObjekt(int[] zahlen) 
+	{
+		this.zahlen = zahlen;
 	}
 
-	protected void start() {
-
+	protected void start() 
+	{
+		//diese Methode soll laut Greini leer sein, höchstens den Status der Wette auf gestartet setzen,
+		//sprich man darf keine Tipps mehr abgeben.
 	}
 
-	protected void end() {
-
+	protected void end() 
+	{
+		//Hier soll die Ziehung stattfinden
 	}
 }
